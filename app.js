7034edaebd7c62ceeecc2e7e0817f77cfde7581f
Daris <daris.hodzic.dh@gmail.com>
2021-09-09 19:04:18 +0200

    finished

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const form = document.querySelector(
 	".search-form"
 );
 let searchValue;
+const more =
+	document.querySelector(".more");
+let page = 1;
+let fetchLink;
+let currentSearch;
 
 //Event Listener
 searchInput.addEventListener(
@@ -18,9 +23,13 @@ searchInput.addEventListener(
 );
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
+	currentSearch = searchValue;
 	searchPhotos(searchValue);
 });
-
+more.addEventListener(
+	"click",
+	loadMore
+);
 //Functions
 
 function updateInput(e) {
@@ -46,23 +55,31 @@ function generatePictures(data) {
 		galleryImg.classList.add(
 			"gallery-img"
 		);
-		galleryImg.innerHTML = `<img src=${photo.src.large}/> 
-        <p>${photo.photographer}</p>`;
+		galleryImg.innerHTML = ` 
+        <div class="gallery-info">
+            <p>${photo.photographer}</p>
+            <a href=${photo.src.original}>Download</a>
+        </div>
+        <img src=${photo.src.large}/>
+        `;
 		gallery.appendChild(galleryImg);
 	});
 }
 
 async function curatedPhotos() {
+	fetchLink =
+		"https://api.pexels.com/v1/curated?per_page=15&page=1";
 	const data = await fetchApi(
-		"https://api.pexels.com/v1/curated?per_page=15&page=1"
+		fetchLink
 	);
 	generatePictures(data);
 }
 
 async function searchPhotos(query) {
 	clear();
+	fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`;
 	const data = await fetchApi(
-		`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
+		fetchLink
 	);
 	generatePictures(data);
 }
@@ -72,4 +89,16 @@ function clear() {
 	searchInput.value = "";
 }
 
+async function loadMore() {
+	page++;
+	if (currentSearch) {
+		fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page=15&page=${page}`;
+	} else {
+		fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+	}
+	const data = await fetchApi(
+		fetchLink
+	);
+	generatePictures(data);
+}
 curatedPhotos();
